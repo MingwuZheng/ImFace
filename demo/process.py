f64ea51f46b5f3905d625231b0d078d5fcbfcedf
aejion <zhyzhy@buaa.edu.cn>
2023-12-29 11:38:42 +0800

    imface++

--- demo/process.py
+++
@@ -1,7 +0,0 @@
-
-import os, sys
-root_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
-sys.path.append(root_dir)
-from data_preprocess.preprocess import data_preprocess
-
-data_preprocess('demo/facescape_demo.obj', 'demo/demo.npy')
